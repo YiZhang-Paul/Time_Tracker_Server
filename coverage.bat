4198a1055633004a669fb7a01b20942a3dc8eee9
YI ZHANG <scaccterran@gmail.com>
2022-01-23 12:35:50 -0500

    TT-6 setup code coverage generation across multiple test projects

--- coverage.bat
+++ coverage.bat
@@ -1,8 +1,3 @@
-dotnet test ./TimeTracker.sln /p:CollectCoverage=true /p:CoverletOutput=../coverage\ /p:CoverletOutputFormat=cobertura
-
-dotnet %userprofile%\.nuget\packages\reportgenerator\5.0.2\tools\netcoreapp3.1\ReportGenerator.dll ^
-    "-reports:coverage/coverage.cobertura.xml" ^
-    "-targetdir:coverage" ^
-    -reporttypes:Html
-
-start .\coverage\index.html\
+dotnet test ./TimeTracker.sln --collect:"XPlat Code Coverage"
+reportgenerator -reports:**/coverage.cobertura.xml -targetdir:coverage
+start .\coverage\index.html
