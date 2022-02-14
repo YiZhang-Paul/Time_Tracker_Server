2e0785256e36f32065bfa421710217c2d335edd7
YI ZHANG <scaccterran@gmail.com>
2022-02-13 19:08:30 -0500

    TT-43 include progress on summaries

--- Core/Dtos/InterruptionItemSummaryDto.cs
+++ Core/Dtos/InterruptionItemSummaryDto.cs
@@ -1,4 +1,6 @@
 using Core.Enums;
+using Core.Models.WorkItem;
+using System.Linq;
 
 namespace Core.Dtos
 {
@@ -7,5 +9,22 @@ namespace Core.Dtos
         public long Id { get; set; }
         public string Name { get; set; }
         public Priority Priority { get; set; }
+        public double Progress { get; set; }
+
+        public static InterruptionItemSummaryDto Convert(InterruptionItem item)
+        {
+            if (item == null)
+            {
+                return null;
+            }
+
+            return new InterruptionItemSummaryDto
+            {
+                Id = item.Id,
+                Name = item.Name,
+                Priority = item.Priority,
+                Progress = item.Checklists.Any() ? (double)item.Checklists.Count(_ => _.IsCompleted) / item.Checklists.Count : 0
+            };
+        }
     }
 }
