dcff5fd32625cffb3488a7f992d2c4be23665058
YI ZHANG <scaccterran@gmail.com>
2022-02-20 23:31:19 -0500

    TT-43 sort checklist entries by rank

--- Service/Repositories/TaskItemRepository.cs
+++ Service/Repositories/TaskItemRepository.cs
@@ -41,7 +41,7 @@ namespace Service.Repositories
 
         public async Task<TaskItem> GetItemById(long id, bool excludeDeleted = true)
         {
-            var query = Context.TaskItem.Include(_ => _.Checklists);
+            var query = Context.TaskItem.Include(_ => _.Checklists.OrderBy(entry => entry.Rank));
 
             if (excludeDeleted)
             {
