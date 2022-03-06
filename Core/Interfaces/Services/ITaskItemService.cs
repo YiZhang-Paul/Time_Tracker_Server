577bbc2aedc569dfad4cb0aab1e7e3756a872886
YI ZHANG <scaccterran@gmail.com>
2022-03-06 10:54:52 -0500

    TT-67 allow searching task items

--- Core/Interfaces/Services/ITaskItemService.cs
+++ Core/Interfaces/Services/ITaskItemService.cs
@@ -2,12 +2,14 @@ using Core.Dtos;
 using Core.Enums;
 using Core.Models.WorkItem;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Core.Interfaces.Services
 {
     public interface ITaskItemService
     {
+        Task<List<TaskItemSummaryDto>> GetItemSummaries(string searchText);
         Task<ItemSummariesDto<TaskItemSummaryDto>> GetItemSummaries(DateTime start);
         Task<TaskItem> CreateItem(TaskItemBase item);
         Task<TaskItem> UpdateItem(TaskItem item, ResolveAction action = ResolveAction.None);
