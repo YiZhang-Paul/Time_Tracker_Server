6aa6d4f8e03e74685ecd6d21ad4d335eed7d3048
YI ZHANG <scaccterran@gmail.com>
2022-04-09 09:35:33 -0400

    TT-19 added guid on user refresh token

---
+++ Core/Migrations/20220409132052_UserRefreshTokenGuid.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    public partial class UserRefreshTokenGuid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Guid",
+                table: "UserRefreshToken",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Guid",
+                table: "UserRefreshToken");
+        }
+    }
+}
